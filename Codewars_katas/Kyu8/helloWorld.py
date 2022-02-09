4da388d63fb1445a15970c298640d0b052ce7a52
Noelia <ncrespipomar@cifpfbmoll.eu>
2022-02-09 19:07:34 +0100

    [✨] Implementado la función helloWorld

---
+++ Codewars_katas/Kyu8/helloWorld.py
@@ -0,0 +1,8 @@
+""" 
+https://www.codewars.com/kata/523b4ff7adca849afe000035
+
+Cree una función simple llamada saludar que devuelva el "hola mundo" más famoso.
+"""
+
+def greet():
+    return 'hello world!'
