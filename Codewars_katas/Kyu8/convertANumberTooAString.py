b6cb711ceb48aaa4466cbc86dcc8c0ad91f1d152
Noelia <ncrespipomar@cifpfbmoll.eu>
2022-02-15 00:03:08 +0100

    [✨] Implementado la función convertANumberTooAS...

---
+++ Codewars_katas/Kyu8/convertANumberTooAString.py
@@ -0,0 +1,16 @@
+"""
+codewars.com/kata/5265326f5fda8eb1160004c8
+
+Necesitamos una función que pueda transformar un número en una cadena.
+
+¿Qué formas de lograr esto conoces?
+
+Ejemplos:
+123 --> "123"
+999 --> "999"
+"""
+
+
+
+def number_to_string(num):
+    return str(num)
